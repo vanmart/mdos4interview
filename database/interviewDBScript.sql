38ee6d2aaa0624cfa524bcd7b8297e33290cb565
Ivan D Martinez Cuesta - Vanmartc <vanmartc@gmail.com>
2017-09-01 10:32:50 -0500

    script for creation of DB mysql

---
+++ database/interviewDBScript.sql
@@ -0,0 +1,8 @@
+
+-- -----------------------------------------------------
+-- Schema interview
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `interview` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `interview` ;
+
+
